f15dd12bb3ff899e068253a7f99ef58659cdbdc7
Kakhaber Mchedlishvili <kakha81@gmail.com>
2023-04-18 20:51:31 +0400

    ADD CAROUSEL

---
+++ src/components/SliderData.js
@@ -0,0 +1,49 @@
+import image1 from '../assets/1.jpeg';
+import image2 from '../assets/2.jpeg';
+import image3 from '../assets/3.jpeg';
+import image4 from '../assets/4.jpeg';
+import image5 from '../assets/5.jpeg';
+import image6 from '../assets/6.jpeg';
+import image7 from '../assets/7.jpeg';
+import image8 from '../assets/8.jpeg';
+import image9 from '../assets/9.jpeg';
+
+export const SliderData = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+];
+
+// export const SliderData = [
+//   {
+//     image:
+//       'https://images.pexels.com/photos/91216/pexels-photo-91216.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+//   {
+//     image:
+//       'https://images.pexels.com/photos/2403402/pexels-photo-2403402.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+//   {
+//     image:
+//       'https://images.pexels.com/photos/6530841/pexels-photo-6530841.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+//   {
+//     image:
+//       'https://images.pexels.com/photos/597667/pexels-photo-597667.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+
+//   {
+//     image:
+//       'https://images.pexels.com/photos/164391/pexels-photo-164391.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+//   {
+//     image:
+//       'https://images.pexels.com/photos/6550357/pexels-photo-6550357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+//   },
+// ];
